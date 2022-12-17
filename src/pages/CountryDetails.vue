4c65a6863d6cbd1d53177419ed902d640e67058d
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-17 16:55:23 +0100

    flags finished

--- src/pages/CountryDetails.vue
+++ src/pages/CountryDetails.vue
@@ -1,29 +1,36 @@
 <template>
-  <img src="#" alt="country flag" style="width: 300px" />
-  <h1>{{ store?.currentCountry?.name?.common }}</h1>
-  <table class="table">
-    <thead></thead>
-    <tbody>
-      <tr>
-        <td style="width: 30%">Capital</td>
-        <td>{{ store?.currentCountry?.capital[0] }}</td>
-      </tr>
-      <tr>
-        <td>Area</td>
-        <td>{{ store?.currentCountry?.area }} km <sup>2</sup></td>
-      </tr>
-      <tr>
-        <td>Borders</td>
-        <td>
-          <ul>
-            <li>
-              <a href="/">Borders</a>
-            </li>
-          </ul>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div v-if="store.currentCountry.capital">
+    <img
+      v-bind:src="`https://flagpedia.net/data/flags/icon/72x54/
+${store.currentCountry.alpha2Code.toLowerCase()}.png`"
+      alt="country flag"
+      style="width: 300px"
+    />
+    <h1>{{ store.currentCountry.name.common }}</h1>
+    <table class="table">
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td style="width: 30%">Capital</td>
+          <td>{{ store.currentCountry.capital[0] }}</td>
+        </tr>
+        <tr>
+          <td>Area</td>
+          <td>{{ store.currentCountry.area }} km <sup>2</sup></td>
+        </tr>
+        <tr>
+          <td>Borders</td>
+          <td>
+            <ul>
+              <li v-for="border in store.currentCountry.borders">
+                <a href="/">{{ border }}</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
